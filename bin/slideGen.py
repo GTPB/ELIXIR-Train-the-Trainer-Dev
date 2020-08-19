b54d1b0f119bb60d1d0aec0fcc3e45b52355142d
XMetaMaker <xmetamaker@gmail.com>
2020-08-19 13:34:07 +0100

    Update slideGen.py

--- bin/slideGen.py
+++ bin/slideGen.py
@@ -112,7 +112,8 @@ slidesFile = open("slides.md", "w")
 
 for fileName in fileList:
     fileParts = fileName.rsplit(".")
-    if len(fileParts) == 2 and fileParts[1] == ".md":
+    print(fileParts)
+    if (len(fileParts) == 2) and (fileParts[1] == ".md"):
         print(fileParts)
         f = open(fileName, "r")
         text = f.read()
