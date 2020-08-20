e85aabbbab12d513474d460c2be8537034b839ef
XMetaMaker <xmetamaker@gmail.com>
2020-08-20 23:27:32 +0100

    Update slidesGen.py

--- bin/slidesGen.py
+++ bin/slidesGen.py
@@ -17,10 +17,9 @@
 
 # ---- Imports ----
 
-# the 'os' library allows for the use of several OS related operations, namely listdir(), which we use in this script
+# The 'os' library allows for the use of several OS related operations, namely listdir(), which we use in this script
 # to access the markdown files in the '_episodes' folder.
 import os
-import shutil
 
 
 # ---- Variables ----
@@ -32,16 +31,20 @@ import shutil
 # This will be interpreted as a comment by the Liquid template language.
 # {%endcomment%}
 #
-# A 'Liquid' comment will be seen by a markdown viewer, but it will not be displayed on the website generated
+# A 'Liquid' comment and its tags will be shown by a markdown viewer, but they will not be displayed on the website generated
 # by Jekyll/GitHub Pages. So, here we are using the 'Liquid' comment to add content that we want people to be
 # able to see on the:
 #
 # 1. GitHub repository (but not on the Carpentries-style website).
 # 2. Slides (but not on the Carpentries-style website).
 
-#'Liquid' tags for a comment (these should not be modified, since they are used by Jekyll
+
+# 'Liquid' tags for a comment (these should not be modified, since they are used by Jekyll
 # and GitHub Pages).
+
+# Tag to open a comment section.
 liquidCommentTag_beginning = "{%comment%}"
+# Tag to close a comment section.
 liquidCommentTag_ending = "{%endcomment%}"
 
 # To add content that is solely meant for the slides, we are placing it inside
@@ -81,14 +84,20 @@ slidesTag = "$$$"
 def extractContent(text, beginningTag, endingTag = None):
     if endingTag == None:
         endingTag = beginningTag
-        
+     
+    # text.find(string) will return the position where the first occurrence of 'string'
+    # begins in the text. It will return -1 if 'string' is not found.
     tagPosition = text.find(beginningTag)
+    
+    # If tagPosition is -1 then that means beginningTag was not found in the text.
     if tagPosition != -1:
     	content_begin = tagPosition + len(beginningTag)
     	content = text[content_begin:]
 
     	tagPosition = content.find(endingTag)
-    	if tagPosition != -1:
+    	
+        # If tagPosition is -1 then that means endingTag was not found in the text.
+        if tagPosition != -1:
     		content = content[:tagPosition]
     		return content
     	else:
@@ -109,7 +118,7 @@ def extractContent(text, beginningTag, endingTag = None):
 
 # Extracting the entire contents of the markdown files (.md) and placing them in a string variable named text.
 folderPath = "_episodes/"
-fileList = os.listdir(folderPath)
+fileList = os.listdir(folderPath).sort()
 text = ""
 slidesContent = ""
 
@@ -127,7 +136,7 @@ for fileName in fileList:
         slidesContent = slidesContent + extractContent(liquidCommentContent, slidesTag)
         
 
-# Write the content of the slides to a file named slides.md
+# Write the content of the slides to a file named slides.md (in a folder also named slides)
 slidesFile = open("slides/slides.md", "w")
 slidesFile.write(slidesContent)        
 slidesFile.close()
