35a76068c7f3dbf3ca583ae7afd3e19020139436
XMetaMaker <xmetamaker@gmail.com>
2020-08-21 00:10:37 +0100

    Update slidesGen.py

--- bin/slidesGen.py
+++ bin/slidesGen.py
@@ -112,7 +112,8 @@ def extractContent(text, beginningTag, endingTag = None):
 
 # Extracting the entire contents of the markdown files (.md) and placing them in a string variable named text.
 folderPath = "_episodes/"
-fileList = os.listdir(folderPath).sort()
+fileList = os.listdir(folderPath)
+fileList.sort()
 text = ""
 slidesContent = ""
 
